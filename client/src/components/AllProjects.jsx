e3faf3fe3b7bc5f861b93e11d4007aeafe7739b7
Tabish <mohdtabishkhan001@gmail.com>
2025-04-13 10:51:12 +0530

    buttons updated

--- client/src/components/AllProjects.jsx
+++ client/src/components/AllProjects.jsx
@@ -69,8 +69,8 @@ function AllProjects({ deleteFunction, className, project }) {
                 <p>{project.projectDescription}</p>
             </CardContent>
             <CardFooter className="w-1/3 gap-2  ">
-                <Button className="bg-white text-black" onClick={() => clickedView(project._id)}>View</Button>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfPaKwDIXYElN8y62tHemR1Y6qJODqNN087dPEApgNJtCrXdw/viewform?usp=sharing" target='_blank' className='flex-col flex items-center ' ><Button >Feedback</Button></a>
+                <Button className="bg-white text-black hover:text-white" onClick={() => clickedView(project._id)}>View</Button>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfPaKwDIXYElN8y62tHemR1Y6qJODqNN087dPEApgNJtCrXdw/viewform?usp=sharing" target='_blank' className='flex-col flex items-center ' ><Button className="  active:bg-[#2d66bd] ">Feedback</Button></a>
 
                 {/* <Button variant={"outline"} onClick={() => deleteProject(project._id)}><Trash2 /></Button> */}
             </CardFooter>
